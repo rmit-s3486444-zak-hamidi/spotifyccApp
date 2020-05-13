2690ef6da655818126b9a440d4bd087406afd5d4
Zak Hamidi <s3486444@student.rmit.edu.au>
2020-05-13 15:29:27 +1000

    UI Change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,11 @@ export default function Hero() {
   return (
     <section className="hero is-primary">
       <div className="hero-body">
+        <center>
         <div className="container">
           <img src="singers.jpg" alt="singers" />
         </div>
+        </center>
       </div>
     </section>
   )

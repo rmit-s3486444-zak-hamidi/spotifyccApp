501a0f2b8a936a97a45e3166abc5934a0c1a2581
Zak Hamidi <s3486444@student.rmit.edu.au>
2020-05-21 13:39:32 +1000

    Added app.yaml

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "e2fe1e358ca0443692c28620512bade8";
-export const redirectUri = "http://spotifyccapp.s3-website-us-east-1.amazonaws.com/content";
+export const redirectUri = "https://cc2020-s3486444.appspot.com/content";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
